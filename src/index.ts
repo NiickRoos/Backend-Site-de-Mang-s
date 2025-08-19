5ac0e81a914705945ed3b4a6745859c76da59c2b
NiickRoos <nicole.roos@estudante.ifms.edu.br>
2025-08-19 08:56:32 -0300

    Aula do dia 19/08

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
 import 'dotenv/config';
-console.log(process.env.DBUSER);
+console.log(process.env.DBHOST);
+import  express from 'express';
+
+ 
+const app = express();
+{
+    app.get('/', (req, res) => {
+    console.log('DBHOST:', process.env.DBHOST);
+    console.log('DBUSER:', process.env.DBUSER);
+    res.send('Olá ' + process.env.DBUSER);
+    });
+}
+const port = process.env.DBPORT;
+app.listen(port, () => {
+    console.log(` A porta que está rodando é ${port}`);
+});
+export default app;
