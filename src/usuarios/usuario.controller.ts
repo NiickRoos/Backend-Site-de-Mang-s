bc7bdd8b60f3e8f4cd17e5aefbbd5ce6290315af
NiickRoos <nicole.roos@estudante.ifms.edu.br>
2025-09-23 12:26:40 -0300

    arrumando erros

--- src/usuarios/usuario.controller.ts
+++ src/usuarios/usuario.controller.ts
@@ -1,19 +1,30 @@
+
+
 import { Request, Response } from "express";
 import { db } from "../database/banco-mongo.js";
 
 class UsuarioController {
   async adicionar(req: Request, res: Response) {
-    const estudante = req.body;
-    const resultado = await db.collection("estudantes").insertOne(estudante);
-    res.status(201).json({ ...estudante, _id: resultado.insertedId });
+    try {
+      const usuario = req.body;
+      const resultado = await db.collection("usuarios").insertOne(usuario);
+      res.status(201).json({ ...usuario, _id: resultado.insertedId });
+    } catch (erro) {
+      console.error("Erro ao adicionar usuário:", erro);
+      res.status(500).json({ erro: "Erro interno ao adicionar usuário." });
+    }
   }
 
   async listar(req: Request, res: Response) {
-    const estudantes = await db.collection("estudantes").find().toArray();
-    res.status(200).json(estudantes);
+    try {
+      const usuarios = await db.collection("usuarios").find().toArray();
+      res.status(200).json(usuarios);
+    } catch (erro) {
+      console.error("Erro ao listar usuários:", erro);
+      res.status(500).json({ erro: "Erro interno ao listar usuários." });
+    }
   }
 }
 
 const usuarioController = new UsuarioController();
-
 export default usuarioController;
