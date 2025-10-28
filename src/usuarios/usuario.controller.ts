24b0511b7218e8990d14b45319f36e1315b56944
NiickRoos <nicole.roos@estudante.ifms.edu.br>
2025-10-28 16:01:44 -0300

    atualização do index.ts

---
+++ src/usuarios/usuario.controller.ts
@@ -0,0 +1,13 @@
+
+import { Request, Response } from "express"
+
+
+
+class UsuarioController {
+    adicionar(req:Request, res:Response) {
+
+    }
+    listar(req:Request, res:Response) {
+    }
+}                               
+export default new UsuarioController()
