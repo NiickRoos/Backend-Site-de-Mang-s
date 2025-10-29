af6b1ca268b6c24a600ed19e3fab2bfb7e870e08
NiickRoos <nicole.roos@estudante.ifms.edu.br>
2025-10-29 11:21:54 -0400

    nicole A1

--- src/rotas/rotas-autenticadas.ts
+++ src/rotas/rotas-autenticadas.ts
@@ -1,8 +1,7 @@
-//rotas Autenticadas para o usuario logado
-
-
 import carrinhoController from "../carrinho/carrinho.js";
+import produtosController from "../produtos/produtos.js";
 import { Auth } from "../middleware/auth.js";
+import { verificaRole } from "../middleware/verificaRole.js";
 import { Router } from "express";
 
 const rotasAutenticadas = Router();
@@ -10,19 +9,15 @@ const rotasAutenticadas = Router();
 // Todas as rotas abaixo precisam de autenticação
 rotasAutenticadas.use(Auth);
 
-// rotasAutenticadas do carrinho
-rotasAutenticadas.post("/carrinho", carrinhoController.adicionar); // Adicionar item ao carrinho
-rotasAutenticadas.get("/carrinho", carrinhoController.listar); // Listar carrinho do usuário logado
-rotasAutenticadas.delete("/carrinho/:id", carrinhoController.removerCarrinho); // Remover carrinho completo
-rotasAutenticadas.put("/carrinho/:id", carrinhoController.atualizarQuantidade);// Atualizar quantidade do item no carrinho
-rotasAutenticadas.delete("/carrinho/:id/item/:itemId", carrinhoController.removerItem); // Remover item específico do carrinho
+// CARRINHO
+rotasAutenticadas.post("/carrinho", carrinhoController.adicionar);
+rotasAutenticadas.get("/carrinho", carrinhoController.listar);
+rotasAutenticadas.delete("/carrinho/:id", carrinhoController.removerCarrinho);
+rotasAutenticadas.put("/carrinho/:id", carrinhoController.atualizarQuantidade);
+rotasAutenticadas.delete("/carrinho/:id/item/:itemId", carrinhoController.removerItem);
 
-
-rotasAutenticadas.get('/listar', carrinhoController.listar);
-rotasAutenticadas.post('/adicionar', carrinhoController.adicionar);
-
-
-//Ainda vamos ter que criar as rotas para carrinho e produtos
-//tarefa para casa:)
+// PRODUTOS
+rotasAutenticadas.get("/produtos", produtosController.listar); // Qualquer usuário logado
+rotasAutenticadas.post("/produtos", verificaRole("admin"), produtosController.adicionar); // Só admin
 
 export default rotasAutenticadas;
